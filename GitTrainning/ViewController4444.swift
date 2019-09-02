399f5563ee3b06759ca3cbb05323e7dfd3f6198f
Ahmed <moharamahmed1@gmail.com>
2019-09-02 15:08:00 +0200

    New

---
+++ GitTrainning/ViewController4444.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController4444.swift
+//  GitTrainning
+//
+//  Created by Mostafa on 9/2/19.
+//  Copyright © 2019 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController4444: UIViewController {
+    @IBOutlet weak var kkkk: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+  
+
+    @IBAction func Action(_ sender: Any) {
+        kkkk.text = "sdjcnbvkdshjb"
+    }
+}
