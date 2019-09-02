bce6eb45a2f7342163ea37da4ad69bacd59730ed
Moahmed <mm@appssquare.com>
2019-09-02 15:01:54 +0200

    trtret

--- GitTrainning/ViewController.swift
+++ GitTrainning/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func action(_ sender: UIButton) {
-        print("This is a Gi dgddg t tutorial")
+        print("This is a Gi dq  shgq    jgjgg t tutorial")
 
         print("This is a Gi dgddg t tutorial")
         
