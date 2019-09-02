a467afefbfe116c9dc12f30feb1538a829a99c28
Moahmed <mm@appssquare.com>
2019-09-02 14:38:36 +0200

    start

---
+++ GitTrainning/srgdfg.swift
@@ -0,0 +1,13 @@
+//
+//  srgdfg.swift
+//  GitTrainning
+//
+//  Created by Mohamed on 9/2/19.
+//  Copyright © 2019 Mohamed. All rights reserved.
+//
+
+import Foundation
+class Model {
+    
+     
+}
